877e40d053c46e2da53884a5dd6380f109d220dc
yukad2 <myuratq@gmail.com>
2024-01-27 01:44:57 +0900

    サイコロで指定値以外を許可しないように

--- bb_s.lua
+++ bb_s.lua
@@ -3,7 +3,7 @@ RegisterCommand('dice', function(source, args, rawCommand)
 
 	local dices, c = tonumber(args[1]), 1
 	if dices then
-		if dices > 3 then
+		if not (dices == 1 or dices == 2 or dices == 3) then
 			TriggerClientEvent('notification', source, 'You can choose a number between 1 and 3 only', 2)
 			return
 		end
